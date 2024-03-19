60334d93ff06e40d067bc8aa94008408aa0db964
Harsh <myemail@example.com>
2024-03-19 10:27:44 +0530

    Added Drizzleorm, user and course schema with config

---
+++ db/schema.ts
@@ -0,0 +1,36 @@
+import { relations } from "drizzle-orm";
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+
+export const courses = pgTable("courses", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  imgSrc: text("img_src").notNull(),
+});
+
+/** The coursesRelations object is used to define the relationships between the courses and userProgress tables
+ *  The many function is used to define a many-to-one relationship between the courses and userProgress tables.
+ */
+export const coursesRelations = relations(courses, ({ many }) => ({
+  userProgress: many(userProgress),
+}));
+
+export const userProgress = pgTable("user_progress", {
+  userId: text("user_id").primaryKey(),
+  userName: text("user_name").notNull().default("User"),
+  userImageSrc: text("user_image_src").notNull().default("/mascot.svg"),
+  activeCourseId: integer("active_course_id").references(() => courses.id, {
+    onDelete: "cascade",
+  }),
+  hearts: integer("hearts").notNull().default(5),
+  points: integer("points").notNull().default(0),
+});
+
+/**
+ * `fields` property specifies that the `activeCourseId` column in the userProgress table should be joined to the *  id column in the courses table.
+ */
+export const userProgressRelations = relations(userProgress, ({ one }) => ({
+  activeCourse: one(courses, {
+    fields: [userProgress.activeCourseId],
+    references: [courses.id],
+  }),
+}));
