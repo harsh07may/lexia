fbfc9ed60f9327f5d00499ea602e040a1a80d6b8
Harsh <myemail@example.com>
2024-03-30 18:20:09 +0530

    Added the gemified header which has progress, hearts. Added zustand.

--- app/(main)/lesson/header.tsx
+++ app/(main)/lesson/header.tsx
@@ -1,8 +1,39 @@
+import { InfinityIcon, X } from "lucide-react";
+
+import { Progress } from "@/components/ui/progress";
+import Image from "next/image";
+
 type Props = {
   hearts: number;
   percentage: number;
   hasActiveSubscription: boolean;
 };
-export const Header = (props: Props) => {
-  return <div>Header</div>;
+export const Header = ({
+  hasActiveSubscription,
+  hearts,
+  percentage,
+}: Props) => {
+  return (
+    <div className="mx-auto flex w-full max-w-[1140px] items-center justify-between gap-x-7 px-10 pt-[20px] lg:pt-[50px]">
+      <X
+        className="cursor-pointer text-slate-500 transition hover:opacity-75"
+        onClick={() => {}}
+      />
+      <Progress value={percentage} />
+      <div className="flex items-center font-bold text-rose-500">
+        <Image
+          className="mr-2"
+          src="./heart.svg"
+          alt="heart"
+          height={28}
+          width={28}
+        />
+        {hasActiveSubscription ? (
+          <InfinityIcon className="h-6 w-6 stroke-[3]" />
+        ) : (
+          hearts
+        )}
+      </div>
+    </div>
+  );
 };
---
+++ app/(main)/lesson/layout.tsx
@@ -0,0 +1,7 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-full flex-col">
+      <div className="flex h-full w-full flex-col">{children}</div>
+    </div>
+  );
+}
